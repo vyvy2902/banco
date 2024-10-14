ac2fec902fcf0911ed229d85f035f330b4858cff
vyvy2902 <vyvy292004@gmail.com>
2024-10-14 03:12:08 -0300

    Criação do ContaService

--- src/main/java/br/uespi/viniciusdias/banco/infrastructure/repository/ContaRepository.java
+++ src/main/java/br/uespi/viniciusdias/banco/infrastructure/repository/ContaRepository.java
@@ -1,7 +1,11 @@
 package br.uespi.viniciusdias.banco.infrastructure.repository;
 
+import br.uespi.viniciusdias.banco.infrastructure.entity.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContaRepository extends JpaRepository {
+import java.util.Optional;
 
+public interface ContaRepository extends JpaRepository<Conta, Long> {
+
+    Optional<Conta> findByNumeroConta(String numeroConta);
 }
--- src/main/java/br/uespi/viniciusdias/banco/service/ContaService.java
+++ src/main/java/br/uespi/viniciusdias/banco/service/ContaService.java
@@ -1,10 +1,67 @@
 package br.uespi.viniciusdias.banco.service;
 
+import br.uespi.viniciusdias.banco.infrastructure.entity.Conta;
 import br.uespi.viniciusdias.banco.infrastructure.repository.ContaRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 public class ContaService {
 
     @Autowired
     private ContaRepository contaRepository;
+
+    public Conta salvar(Conta conta) {
+        return contaRepository.save(conta);
+    }
+
+    public Optional<Conta> buscarPorId(Long id) {
+        return contaRepository.findById(id);
+    }
+
+    public List<Conta> buscarTodas() {
+        return contaRepository.findAll();
+    }
+
+    public void deletar(Long id) {
+        contaRepository.deleteById(id);
+    }
+
+    public Optional<Conta> buscarPorNumeroConta(String numeroConta) {
+        return contaRepository.findByNumeroConta(numeroConta);
+    }
+
+    @Transactional
+    public void depositar(Long id, BigDecimal valor) {
+        Optional<Conta> contaOpt = buscarPorId(id);
+
+        if (contaOpt.isPresent() && valor.compareTo(BigDecimal.ZERO) > 0) {
+            Conta conta = contaOpt.get();
+            conta.setSaldo(conta.getSaldo().add(valor));
+            salvar(conta);
+        }else {
+            throw new IllegalArgumentException("Valor de depósito deve ser maior que zero ou conta não encontrada.");
+        }
+
+    }
+
+    @Transactional
+    public void sacar(Long id, BigDecimal valor) {
+        Optional<Conta> contaOpt = buscarPorId(id);
+        if (contaOpt.isPresent()) {
+            Conta conta = contaOpt.get();
+            if (valor.compareTo(BigDecimal.ZERO) > 0 && valor.compareTo(conta.getSaldo()) <= 0) {
+                conta.setSaldo(conta.getSaldo().subtract(valor));
+                salvar(conta);
+            } else {
+                throw new IllegalArgumentException("Saque deve ser maior que zero e menor ou igual ao saldo.");
+            }
+        } else {
+            throw new IllegalArgumentException("Conta não encontrada.");
+        }
+    }
+
 }
