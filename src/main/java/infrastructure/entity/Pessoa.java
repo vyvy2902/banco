4dd4d3111e185d22412040e124639b89008fea23
vyvy2902 <vyvy292004@gmail.com>
2024-10-14 02:37:42 -0300

    Entidade Pessoa

--- src/main/java/infrastructure/entity/Pessoa.java
+++ src/main/java/infrastructure/entity/Pessoa.java
@@ -1,4 +1,38 @@
 package infrastructure.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length =  100)
+    private String nome;
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+
+    @ManyToMany
+    @JoinTable(
+        name = "pessoa_conta",
+        joinColumns = @JoinColumn(name = "pessoa_id"),
+        inverseJoinColumns = @JoinColumn(name = "conta_id")
+    )
+    private List<Conta> contas;
 }
