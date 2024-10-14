31f83cbfb88666cef63453196940547f1a549d4a
vyvy2902 <vyvy292004@gmail.com>
2024-10-14 02:42:58 -0300

    Entidade Conta

--- src/main/java/infrastructure/entity/Conta.java
+++ src/main/java/infrastructure/entity/Conta.java
@@ -1,4 +1,28 @@
 package infrastructure.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
 public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String numeroConta;
+    @Column(nullable = false)
+    private BigDecimal saldo;
+    @ManyToMany(mappedBy = "contas")
+    private List<Pessoa> donosConta;
 }
